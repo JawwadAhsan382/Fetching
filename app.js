0a86836872618d16d4caa928b1d33506b84dac7c
JawwadAhsan382 <jawwad654321@gmail.com>
2025-03-12 01:13:34 +0500

    bill added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var getRow=document.querySelector('.row')
 var getWindow=document.querySelector('.window')
 var getCartBut=document.querySelector('.cart-but')
+var getCart=document.querySelector('.cart')
+var getBill=document.querySelector('.bill')
 getCartBut.disabled=true
 var getBadge=document.querySelector('.badge')
 var getSearch=document.querySelector('.srch')
@@ -25,7 +27,7 @@ fetch('https://fakestoreapi.com/products')
         getRow.innerHTML+=`<div class="col-10 col-sm-5 col-md-4 py-2"><div class="card"><div class="card-body">
               <h5 class="card-title">${cv.category}</h5>
               <img src="${cv.image}" width="90%" height="200px" alt="">
-              <p class="card-text">ID: ${cv.id} <br> Price: ${cv.price}$ <br> Title:<br> ${cv.title}</p>
+              <p class="card-text">ID: ${cv.id} <br> Price: <span class="price-${ci}">${cv.price}</span>$ <br> Title:<br> ${cv.title}</p>
               <button value=${ci} class="btn btn-primary" onclick="addToCart(this)">Add to cart</button></div></div></div>`
     })
 })
@@ -41,6 +43,9 @@ function addToCart(a){
     getBadge.innerHTML=getWindow.childNodes.length
     getBadge.style.display='inline'
     a.disabled=true
+    getBill.innerText=Array.from(getWindow.childNodes).reduce((acc,cv)=>{
+        return acc+Number(document.querySelector('.price-'+cv.lastChild.lastChild.value).innerText)
+    },0).toFixed(2)
 }
 function removeFromCart(a){
     a.parentNode.parentNode.remove()
@@ -49,17 +54,20 @@ function removeFromCart(a){
     if(getWindow.childNodes.length==0){
         getCartBut.disabled=true
         getBadge.style.display='none'
-        getWindow.style.display='none'
+        getCart.style.display='none'
         getCartBut.value=0
     }
+    getBill.innerText=Array.from(getWindow.childNodes).reduce((acc,cv)=>{
+        return acc+Number(document.querySelector('.price-'+cv.lastChild.lastChild.value).innerText)
+    },0).toFixed(2)
 }
 function appear(){
     if(getCartBut.value==0){
-        getWindow.style.display='flex'
+        getCart.style.display='flex'
         getCartBut.value=1
     }
     else{
-        getWindow.style.display='none'
+        getCart.style.display='none'
         getCartBut.value=0
     }
 }
